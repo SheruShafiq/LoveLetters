a5cd7a84645a1f0165d1c976aaaad84785607ef7
Sheru <100682913+SheruShafiq@users.noreply.github.com>
2024-11-03 14:44:45 +0100

    added composer to docker conf used composer to  use dotenv setted up xdebug

--- backend/src/index.php
+++ backend/src/index.php
@@ -1,2 +1,23 @@
 <?php
-echo "Hello from PHP!";
+require 'vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+
+$host = 'db';
+$db   = $_ENV['POSTGRES_DB'];
+$user = $_ENV['POSTGRES_USER'];
+$pass = $_ENV['POSTGRES_PASSWORD'];
+$dsn = "pgsql:host=$host;port=5432;dbname=$db;";
+
+try {
+    $conn = new PDO($dsn, $user, $pass);
+    echo "Connected to the database successfully!";
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+
