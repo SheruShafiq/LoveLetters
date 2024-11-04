4790eb875cc151c05dbf4db3a2286266b96e9d4e
Sheru <100682913+SheruShafiq@users.noreply.github.com>
2024-11-04 08:22:36 +0100

    CORS error dev env fix

--- backend/src/index.php
+++ backend/src/index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-header('Access-Control-Allow-Origin: http://localhost:5173'); // Adjust this to your frontend's URL
+header('Access-Control-Allow-Origin: http://localhost:5173');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Access-Control-Allow-Credentials: true');
